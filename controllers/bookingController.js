530214a7462ccb3af80a1e9412be5d283ff49943
=Mehmet <=>
2021-12-14 06:46:41 +0300

    sorunu hallettik sayılır

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -6,34 +6,33 @@ const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
-  // 1) Get the currently booked tour
+  // !) Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId);
- 
-  // 2) Create Checkout session
-  const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+  
+  // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
+    //   req.params.tourId
+    // }&user=${req.user.id}&price=${tour.price}`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
     line_items: [
       {
-        quantity: 1,
-        price_data: {
-          currency: 'usd',
-          unit_amount: tour.price * 100,
-          product_data: {
-            name: `${tour.name} Tour`,
-            description: tour.summary,
-            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`]
-          }
-        }
+        name: `${tour.name} Tour`,
+        description: tour.summary,
+        images: [
+          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`
+        ],
+        amount: tour.price * 100,
+        currency: 'usd',
+        quantity: 1
       }
     ]
   });
- 
-  // 3) Create Session as response
+  // 3) create session as response
   res.status(200).json({
     status: 'success',
     session
@@ -51,10 +50,10 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 //   res.redirect(req.originalUrl.split('?')[0]);
 // });
 
-const createBookingCheckout = async session => {
+const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const { price } = session.line_items[0].price_data.unit_amount / 100;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
 
