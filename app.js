62758423d83e8c01618e7df306c9c7edbd0a9ea8
=Mehmet <=>
2021-12-14 05:18:20 +0300

    Improve stripe implementation

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRouter');
+const bookingController = require('./controllers/bookingController');
 const viewRouter = require('./routes/viewRoutes');
 
 //
@@ -70,6 +71,12 @@ const limiter = rateLimit({
 });
 
 app.use('/api', limiter); // all root
+
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 
 //Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
