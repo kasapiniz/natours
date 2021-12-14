f1bc57f9965d82ea3d627c04c549170a9811afec
=Mehmet <=>
2021-12-14 04:51:14 +0300

    modified app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
-const cors= require('cors');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -29,7 +29,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 // 1.GLOBAL  middleware
 //Implement CORS
-app.us(cors());
+app.use(cors());
 // Access-Control-Allow-Origin *
 // api.natours.com, natours.com, natours.com
 // app.use(cors({
