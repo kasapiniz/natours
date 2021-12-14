4b743cfebae48b53438fd9ece8d8b0241e00acd7
=Mehmet <=>
2021-12-14 05:52:51 +0300

    deneme

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -15,7 +15,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     // success_url: `${req.protocol}://${req.get('host')}/?tour=${
     //   req.params.tourId
     // }&user=${req.user.id}&price=${tour.price}`,
-    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+    success_url: `${req.protocol}://${req.get('host')}/?tour`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -55,7 +55,6 @@ const createBookingCheckout = async session => {
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.display_items[0].amount / 100;
   await Booking.create({ tour, user, price });
-
 };
 
 exports.webhookCheckout = (req, res, next) => {
