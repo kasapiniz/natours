513f2f5662caabf398b56b7b38fe0d1c36bb2fbb
=Mehmet <=>
2021-12-14 06:59:17 +0300

    alertmessage

--- public/js/index.js
+++ public/js/index.js
@@ -88,4 +88,4 @@ if (bookBtn)
 
 
   const alertMessage = document.querySelector('body').dataset.alert;
-  if(alert) showAlert('success',alertMessage,20)
+  if(alertMessage) showAlert('success',alertMessage,20)
